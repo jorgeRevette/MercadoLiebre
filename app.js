f1fe95178229c2f310257b101b6c76535fcb7049
jorgeRevette <jorge.16.vall@gmail.com>
2023-01-25 18:16:16 -0300

    subiendo a render

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-// const express = require('express');
-//  const app = express();
-//  const path = require('path');
-// const port = 3000;
-
-// app.use(express.static('public'));
-
-// app.get('/', (req, res)=>{
-//    res.sendFile(
-//     path.join(__dirname, './views/home.html')
-//    );
-// });
-
-// app.listen(port, () =>{
-//     console.log('lavantando puerto' + port)
-// });
-
 
 const express = require('express');
 const path = require("path");
